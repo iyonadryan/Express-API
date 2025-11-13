69e7f443c0f9f3765813f8b5026fed8d0647e960
Iyon AdryanLF <adryaniyon@gmail.com>
2025-11-13 21:25:37 +0700

    ROGRESS #2 (nodemon + body-parser)

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+  console.log({ loginRequestFromOutside: req.body})
+  const username = req.body.username
+  res.send('login berhasil, Hallo ' + username )
 })
 
 app.listen(port, () => {
